6f3dd76618506475aab7e935c06652c50c25e243
YinYin Chiu <cychiuae@connect.ust.hk>
2019-12-10 12:37:43 +0800

    Define CasbinRule model

---
+++ pkg/model/casbin.go
@@ -0,0 +1,13 @@
+package model
+
+// CasbinRule is the model for casbin rule
+type CasbinRule struct {
+	PType string
+	V0    string
+	V1    string
+	V2    string
+	V3    string
+	V4    string
+	V5    string
+}
+
