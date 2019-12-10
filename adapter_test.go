cef7f19767b5218cf6237801ae4c342344b26988
YinYin Chiu <cychiuae@connect.ust.hk>
2019-12-10 16:03:11 +0800

    Add a test to test the adapter

--- adapter_test.go
+++ adapter_test.go
@@ -3,12 +3,77 @@ package casbinpgadapter
 import (
 	"os"
 	"testing"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/util"
 )
 
+// TestAdapter is a very bad all-in-one integration test to test the adapter
 func TestAdapter(t *testing.T) {
-	connectionString := os.Getenv("DATABASE_URL")
-	adapter, err := NewAdapter(connectionString, "casbin")
-	if adapter == nil || err != nil {
-		t.Errorf("Cannot create adapter adatper: %v err: %v", adapter, err)
+	enforcer, err := casbin.NewEnforcer("./example/model.conf", "./example/policy.csv")
+	if err != nil {
+		t.Fatal("Cannot create enforcer")
+		return
+	}
+	adapter, err := NewAdapter(os.Getenv("DATABASE_URL"), "casbin")
+	if err != nil {
+		t.Fatalf("Cannot create adapter %v", err)
+		return
+	}
+	if err = adapter.SavePolicy(enforcer.GetModel()); err != nil {
+		t.Fatalf("Cannot initial policy %v", err)
+		return
+	}
+
+	if err != nil {
+		t.Fatalf("Cannot create adapter %v", err)
+		return
+	}
+
+	adapter, err = NewAdapter(os.Getenv("DATABASE_URL"), "casbin")
+	enforcer, err = casbin.NewEnforcer("./example/model.conf", adapter)
+	enforcerPolicy := enforcer.GetPolicy()
+	want := [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}}
+	if !util.Array2DEquals(enforcerPolicy, want) {
+		t.Fatalf("Want %v but got %v", want, enforcerPolicy)
+		return
+	}
+
+	enforcer.EnableAutoSave(false)
+	enforcer.AddPolicy("alice", "data1", "write")
+	enforcer.LoadPolicy()
+	enforcerPolicy = enforcer.GetPolicy()
+	if !util.Array2DEquals(enforcerPolicy, want) {
+		t.Fatalf("Want %v but got %v", want, enforcerPolicy)
+		return
+	}
+
+	enforcer.EnableAutoSave(true)
+
+	enforcer.AddPolicy("alice", "data1", "write")
+	enforcer.LoadPolicy()
+	enforcerPolicy = enforcer.GetPolicy()
+	want = [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}, {"alice", "data1", "write"}}
+	if !util.Array2DEquals(enforcerPolicy, want) {
+		t.Fatalf("Want %v but got %v", want, enforcerPolicy)
+		return
+	}
+
+	enforcer.RemovePolicy("alice", "data1", "write")
+	enforcer.LoadPolicy()
+	enforcerPolicy = enforcer.GetPolicy()
+	want = [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}}
+	if !util.Array2DEquals(enforcerPolicy, want) {
+		t.Fatalf("Want %v but got %v", want, enforcerPolicy)
+		return
+	}
+
+	enforcer.RemoveFilteredPolicy(0, "data2_admin")
+	enforcer.LoadPolicy()
+	enforcerPolicy = enforcer.GetPolicy()
+	want = [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}}
+	if !util.Array2DEquals(enforcerPolicy, want) {
+		t.Fatalf("Want %v but got %v", want, enforcerPolicy)
+		return
 	}
 }
